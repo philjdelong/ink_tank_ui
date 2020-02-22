2b76095c8f9808cbb0f816062a045262ea89bf20
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-22 12:40:40 -0700

    add shop and user models

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,8 @@
 class Shop < ApplicationRecord
   has_many :users
+
+  validates_presence_of :street_address
+  validates_presence_of :city
+  validates_presence_of :phone_number
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
 class User < ApplicationRecord
   belongs_to :shop
+
+  def update_user(user_info)
+    current_user.uid = user_info[:uid]
+    current_user.token = user_info[:credentials][:token]
+    current_user.login = user_info[:extra][:raw_info][:login]
+    current_user.save!
+  end
+
+  def self.create_user(shop, user_info)
+    User.create!( uid:     user_info[:uid],
+                  shop_id: shop.id,
+                  uid:     user_info[:uid],
+                  token:   user_info[:credentials][:token],
+                  login:   user_info[:extra][:raw_info][:login])
+  end
 end
