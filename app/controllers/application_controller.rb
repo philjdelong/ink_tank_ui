e4f2b6f785670773a6dbf4006ef65ad6902bcbd7
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-22 12:39:42 -0700

    create current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def current_user
+    @current_user ||= User.find_by(uid: session[:github]) if session[:github]
+  end
 end
+
+
+
+# create default shop (seed) - done
+# when creating user, set shop_id to default shop - done
+# update user's shop_id once user has completed the shop flow
