d26c1e3f3f553c524f5358b9d8e824fe85e71ac4
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-22 12:36:38 -0700

    add shop users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/', to: 'welcome#index'
+
+  get 'auth/github', as: 'provider_name_login'
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  resources :shops, only: [:show, :new, :index, :create] do
+    resources :users, only: [:new, :create, :show, :update]
+  end
 end
