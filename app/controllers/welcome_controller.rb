56803dc687bc3c6e7ab6226f263d469b3f4e7a59
Phil DeLong <philjdelong@gmail.com>
2020-02-22 19:31:12 -0700

    Include index method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,10 @@
 class WelcomeController < ApplicationController
+   
    def index
-      
+      return @image_results_list if @image_results_list
+      if params[:search_tattoos]
+         image_search = GoogleSearchResults.new(q: "tattoo #{ params[:search_tattoos] }", tbm: "isch", serp_api_key: ENV['SERP_API_KEY'])
+         @image_results_list = image_search.get_hash[:images_results][0..9]
+      end
    end
 end
