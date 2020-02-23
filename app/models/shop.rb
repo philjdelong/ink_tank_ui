005c3e72647770f2dfab3e77aa86cd678f26edab
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-23 11:01:50 -0700

    add create shop method

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,8 +1,18 @@
 class Shop < ApplicationRecord
   has_many :users
 
+  validates_presence_of :name
   validates_presence_of :street_address
   validates_presence_of :city
   validates_presence_of :phone_number
+  validates_presence_of :zip
 
+  def self.create_shop(params)
+    Shop.create(
+      street_address: params["/shops"][:street_address],
+      name: params["/shops"][:name],
+      city: params["/shops"][:city],
+      phone_number: params["/shops"][:phone_number],
+      zip: params["/shops"][:zip])
+  end
 end
