799300beb4901bcb718056ac2c4fc88d43c0471d
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-23 10:58:17 -0700

    edit create_user method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,3 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find_by(uid: session[:github]) if session[:github]
   end
 end
-
-
-
-# create default shop (seed) - done
-# when creating user, set shop_id to default shop - done
-# update user's shop_id once user has completed the shop flow
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,23 @@
 class User < ApplicationRecord
   belongs_to :shop
 
-  def update_user(user_info)
+  def update_user(user_info, current_user)
     current_user.uid = user_info[:uid]
     current_user.token = user_info[:credentials][:token]
     current_user.login = user_info[:extra][:raw_info][:login]
-    current_user.save!
+    current_user.save
   end
 
-  def self.create_user(shop, user_info)
+  def self.create_user(user_info)
+    shop = Shop.first
     User.create!( uid:     user_info[:uid],
                   shop_id: shop.id,
                   uid:     user_info[:uid],
                   token:   user_info[:credentials][:token],
                   login:   user_info[:extra][:raw_info][:login])
+  end
+
+  def complete_user(params)
+    require "pry"; binding.pry
   end
 end
