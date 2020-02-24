8be5f8aa53a2c4d550877000362a6708b7135ba9
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-23 18:38:41 -0700

    refactor new method to use locals

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,25 +1,23 @@
 class ShopsController < ApplicationController
 
   def new
+    @shop = Shop.new
     if params["/shops"]
-      @new_shop = Shop.new(zip: params["/shops"][:zip_code])
-      if Shop.exists?(zip: params["/shops"][:zip_code])
-        @shops = Shop.where(zip: params["/shops"][:zip_code])
-      else
-        flash[:notice] = 'No shops found in that zipcode. Please create one.'
-      end
+      render locals: { new_shop: Shop.new(zip: params["/shops"][:zip_code]),
+                       shops: Shop.where(zip: params["/shops"][:zip_code])}
+    else
+      flash[:notice] = 'No shops found in that zipcode. Please create one.'
     end
   end
 
   def create
-    #investigate why shop_params are returning as blank, used model method as workaround
-    shop = Shop.create_shop(params)
-    if shop.save
-      current_user.update_user_shop(current_user, shop.id)
+    @shop = Shop.create(shop_params)
+    if @shop.save
+      current_user.update_user_shop(current_user, @shop.id)
       flash[:notice] = 'Shop successfully created'
       redirect_to new_user_path
     else
-      flash[:error] = shop.errors.full_messages.to_sentence
+      flash[:error] = @shop.errors.full_messages.to_sentence
     end
   end
 
@@ -33,6 +31,6 @@ class ShopsController < ApplicationController
 
 
   def shop_params
-    params.permit(:street_address, :city, :phone_number, :zip)
+    params.require(:shop).permit(:name, :street_address, :city, :phone_number, :zip)
   end
 end
