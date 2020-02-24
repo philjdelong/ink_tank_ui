d0728c0294d721a83c34a6227a1ffb81c54a732c
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-23 18:39:11 -0700

    remove method after refactor

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -7,12 +7,4 @@ class Shop < ApplicationRecord
   validates_presence_of :phone_number
   validates_presence_of :zip
 
-  def self.create_shop(params)
-    Shop.create(
-      street_address: params["/shops"][:street_address],
-      name: params["/shops"][:name],
-      city: params["/shops"][:city],
-      phone_number: params["/shops"][:phone_number],
-      zip: params["/shops"][:zip])
-  end
 end
