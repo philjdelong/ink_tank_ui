cf0eac2e148edc0352300e86506394c0afc08ef7
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-24 16:19:49 -0700

    add google oauth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def current_user
-    @current_user ||= User.find_by(uid: session[:github]) if session[:github]
+    @current_user ||= User.find_by(uid: session[:login]) if session[:login]
   end
 end
