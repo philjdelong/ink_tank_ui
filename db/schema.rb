defad5ab6e9e87740eff3b150eb72eee5f5a463a
Phil DeLong <philjdelong@gmail.com>
2020-02-24 20:47:43 -0700

    Update schema and seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_215158) do
+ActiveRecord::Schema.define(version: 2020_02_25_015052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 2020_02_24_215158) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "styles", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,15 @@
-
+Style.destroy_all
 default_shop = Shop.create!(name: 'Default shop', street_address: '123 Main', city: 'Denver', zip: '80206', phone_number: '123456789')
+
+@style_1 = Style.create(
+   name: "Black and White",
+   description: "Super cool stuff right here"
+)
+@style_2 = Style.create(
+   name: "Traditional",
+   description: "Super traditional stuff, Yep."
+)
+@style_3 = Style.create(
+   name: "Tribal",
+   description: "Arm band."
+)
