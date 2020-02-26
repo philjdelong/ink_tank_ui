b6db0908241991c6c9b24f497e10fa9f08754123
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-26 16:52:31 -0700

    remove commented out lines

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,6 @@ class SessionsController < ApplicationController
     if Artist.exists?(uid: user_info[:uid]) == false
       Artist.save_user_oauth_info(user_info)
       redirect_to new_shop_path
-    # elsif user.name == ''
-    #  user = Artist.find_by(uid: user_info[:uid])
-    #  redirect_to user_path
     else
       Artist.update_user(user_info, current_user)
       redirect_to profile_path
