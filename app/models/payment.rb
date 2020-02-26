6a822b393e8a52019fac0620273474a91641aeff
Wren Steitle <lauren.steitle@gmail.com>
2020-02-26 11:12:38 -0700

    Create payments migration

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,5 +1,5 @@
 class Payment < ApplicationRecord
   belongs_to :artist
 
-
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_015052) do
+ActiveRecord::Schema.define(version: 2020_02_26_181019) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "payments", force: :cascade do |t|
+    t.string "name"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.string "phone_number"
+    t.string "status"
+    t.string "amount"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_payments_on_user_id"
+  end
 
   create_table "shops", force: :cascade do |t|
     t.string "name"
@@ -43,5 +58,6 @@ ActiveRecord::Schema.define(version: 2020_02_25_015052) do
     t.index ["shop_id"], name: "index_users_on_shop_id"
   end
 
+  add_foreign_key "payments", "users"
   add_foreign_key "users", "shops"
 end
