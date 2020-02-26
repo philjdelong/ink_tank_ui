53fa74a4805079305dd469f77ffec7d093f0ab38
Lain McGratH <lain.mcgrath@gusto.com>
2020-02-25 22:04:30 -0700

    Add sessions controller refactor wip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,12 @@ class SessionsController < ApplicationController
     user_info = request.env['omniauth.auth']
     session[:login] = user_info[:uid]
     if User.exists?(uid: user_info[:uid]) == false
-      User.create_user(user_info)
+      User.save_user_oauth_info(user_info)
+      require "pry"; binding.pry
       redirect_to new_shop_path
+    # elsif user.name == ''
+    #  user = User.find_by(uid: user_info[:uid])
+    #  redirect_to user_path
     else
       User.update_user(user_info, current_user)
       redirect_to profile_path
