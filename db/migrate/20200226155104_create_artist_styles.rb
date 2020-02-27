223a0eb2796a78d05b3d65fa5bcc4c0f5defe4ab
Phil DeLong <philjdelong@gmail.com>
2020-02-27 11:42:48 -0700

    Update naming convention

--- db/migrate/20200226155104_create_artist_styles.rb
+++
@@ -1,8 +0,0 @@
-class CreateArtistStyles < ActiveRecord::Migration[5.2]
-  def change
-    create_table :artist_styles do |t|
-      t.references :user, foreign_key: true
-      t.references :style, foreign_key: true
-    end
-  end
-end
