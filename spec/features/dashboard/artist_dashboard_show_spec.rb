1b96bf88e4cb33ef8bd24075b384342336accc60
Phil DeLong <philjdelong@gmail.com>
2020-02-27 16:17:11 -0700

    Start test

---
+++ spec/features/dashboard/artist_dashboard_show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe "As a registered user" do
+    describe "i can click a link in the nav" do
+        it "to visit my dashboard" do
+
+        end
+    end 
+end
