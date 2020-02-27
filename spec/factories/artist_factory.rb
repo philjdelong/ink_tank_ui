b5ea621b8b84face5afc51017a43a530fc9eeeea
Phil DeLong <philjdelong@gmail.com>
2020-02-27 12:06:34 -0700

    Add needed fields to artist

--- spec/factories/artist_factory.rb
+++ spec/factories/artist_factory.rb
@@ -2,7 +2,9 @@ FactoryBot.define do
   factory :artist do
     name { "Stabby Lambda" }
     price_per_hour { 150.00 }
-    bio { "Tattooer. Stayed at a Holiday Inn Express once." }
-   )
+    bio { 'Tattooer. Stayed at a Holiday Inn Express once.' }
+    uid { '11111' }
+    login { 'example@mail.com' }
+    token { 'token' }
   end
 end
