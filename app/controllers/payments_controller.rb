796c85f44635e58ea1b5badd64ed19ed6cce8d7d
Wren Steitle <lauren.steitle@gmail.com>
2020-02-27 13:25:57 -0700

    Clean up controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,17 +1,6 @@
 class PaymentsController < ApplicationController
 
   def new
-  end
-
-  def create
-    amount = (params[:amount].to_f * 100)
-    raw_charge = Faraday.get("http://localhost:9292/payments") do |req|
-      req.body = {title: "#{params[:title]}",
-                description: "#{params[:description]}",
-                amount: "#{amount}"}.to_json
-    end
-
-    charge = JSON.parse(raw_charge.body)
-    redirect_to '/profile'
+    @amount = (params[:amount].to_f * 100).to_i
   end
 end
