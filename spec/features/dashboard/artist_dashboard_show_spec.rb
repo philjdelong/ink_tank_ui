24d73d6aacf4d5a4823a7afaf2d0e82c4dfe517a
Phil DeLong <philjdelong@gmail.com>
2020-02-27 22:41:26 -0700

    Commit to change branch

--- spec/features/dashboard/artist_dashboard_show_spec.rb
+++ spec/features/dashboard/artist_dashboard_show_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "As a registered user" do
     describe "i can click a link in the nav" do
         it "to visit my dashboard" do
             
+            
         end
     end 
 end
