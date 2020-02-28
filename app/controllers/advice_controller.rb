b82be3902654d1757d2811400c8d2c0577795b67
Wren Steitle <lauren.steitle@gmail.com>
2020-02-28 08:46:43 -0700

    Complete

--- app/controllers/advice_controller.rb
+++ app/controllers/advice_controller.rb
@@ -1,6 +1,6 @@
 class AdviceController < ApplicationController
   def show
-    advice = Faraday.get('https://ink-tank-be.herokuapp.com')
+    advice = Faraday.get('https://ink-tank-be.herokuapp.com/advice')
     @advice = JSON.parse(advice.body)
   end
 end
