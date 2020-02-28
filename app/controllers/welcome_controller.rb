a006c7c8227bb98afb085603943af76a9a730603
Phil DeLong <philjdelong@gmail.com>
2020-02-27 22:25:37 -0700

    Update

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,8 +3,8 @@ class WelcomeController < ApplicationController
    def index
       return @image_results_list if @image_results_list
       if params[:search_tattoos]
-         image_search = GoogleSearchResults.new(q: "tattoo #{ params[:search_tattoos] }", tbm: "isch", serp_api_key: ENV['SERP_API_KEY'])
-         @image_results_list = image_search.get_hash[:images_results][0..9]
+         image_search = GoogleSearchResults.new(q: "#{ params[:search_tattoos] } tattoos", tbm: "isch", serp_api_key: ENV['SERP_API_KEY'])
+         @image_results_list = image_search.get_hash[:images_results][0..8]
       end
    end
 end
