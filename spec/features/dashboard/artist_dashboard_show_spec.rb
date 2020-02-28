79a95e7a20d9476a9e70d58b1b55e2e32a723d79
Phil DeLong <philjdelong@gmail.com>
2020-02-27 20:22:00 -0700

    Create test

--- spec/features/dashboard/artist_dashboard_show_spec.rb
+++ spec/features/dashboard/artist_dashboard_show_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "As a registered user" do
     describe "i can click a link in the nav" do
         it "to visit my dashboard" do
-
+            
         end
     end 
 end
